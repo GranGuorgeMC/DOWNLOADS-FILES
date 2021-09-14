a7aae15774c2b419b103d06a2f6768b04a959db7
GranGuorgeMC <90460977+GranGuorgeMC@users.noreply.github.com>
2021-09-14 08:17:14 +0800

    Update mcpe.sh

--- mcpe.sh
+++ mcpe.sh
@@ -1,10 +1,8 @@
-echo "downloading the server"
-wget https://github.com/GranGuorgeMC/DOWNLOADS-FILES/releases/download/1.17.11/minecarft.zip
-unzip minecarft.zip
 echo "Downloading playit.gg"
 wget https://playit.gg/downloads/playit-linux_64-0.4.7
 chmod +x playit-linux_64-0.4.7
 echo "=================================="
+echo "run screen" 
 echo "run ./playit-linux_64-0.4.7" 
 echo "=================================="
 
