7144d58d15afb877b148b8f1db231fc3cdf6e44f
GranGuorge <90460977+GranGuorgeMC@users.noreply.github.com>
2021-10-17 20:47:43 +0800

    Create pack.sh

---
+++ pack.sh
@@ -0,0 +1,8 @@
+cd bedrock
+cd worlds
+cd 'Bedrock level'
+cd resource_packs
+wget https://github.com/GranGuorgeMC/DOWNLOADS-FILES/releases/download/1.17.11/GRANGuore.zip
+unzip GRANGuore.zip
+rm GRANGuore.zip
+echo 'done😃'
