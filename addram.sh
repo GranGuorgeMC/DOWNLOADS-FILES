52a54d581202fdcc7bb654a52e0eeeeab628972c
GranGuorge <90460977+GranGuorgeMC@users.noreply.github.com>
2022-04-25 12:08:28 +0800

    Create addram.sh

---
+++ addram.sh
@@ -0,0 +1,26 @@
+echo "starting.."
+echo "You must be in root user"
+sudo su
+echo "note: You Cant Repeat This process"
+echo "How much"
+read -p "ram: " CRP
+echo "===================================="
+echo "adding $CRP ram"
+echo "===================================="
+sudo dd if=/dev/zero of=/mnt/swap.0 bs=$CPR count=1048576
+echo "=======25%"
+sudo mkswap /mnt/swap.0
+echo "=============50%"
+echo "/mnt/swap.0 swap swap defaults 0 0" >> /etc/fstab
+echo "=================70%"
+swapon /mnt/swap.0
+echo "=====================90%"
+sudo swapon -s
+clear
+echo "=======================100%"
+echo "===================================="
+free m
+echo "===================================="
+echo "ram added $CRP mb"
+echo "clearing trash"
+echo "done"
