23cb145aa16abea2371c453bb9297393cfb63637
GranGuorge <90460977+GranGuorgeMC@users.noreply.github.com>
2022-04-25 11:48:17 +0800

    Add files via upload

---
+++ Bot/ramadd.sh
@@ -0,0 +1,29 @@
+echo "starting.."
+sleep 5s
+echo "You must be in root user"
+sudo su
+echo "note: You Cant Repeat This process"
+echo "How much"
+read -p "ram:" COUNT
+echo "===================================="
+echo "adding $COUNT ram"
+echo "===================================="
+sleep 4s
+sudo dd if=/dev/zero of=/mnt/swap.0 bs=$COUNT count=1048576
+echo "=======25%"
+sudo mkswap /mnt/swap.0
+echo "=============50%"
+echo "/mnt/swap.0 swap swap defaults 0 0" >> /etc/fstab
+echo "=================70%"
+swapon /mnt/swap.0
+echo "=====================90%"
+sudo swapon -s
+clear
+echo "=======================100%"
+echo "===================================="
+free m
+echo "===================================="
+echo "ram added $COUNT mb"
+echo "clearing trash"
+sleep 5s
+echo "done"
